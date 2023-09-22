67be3d8f843e4a71b8a19b423560226989334ec5
Grzegorz Dubiel <greg2012201@gmail.com>
2023-09-23 01:26:30 +0200

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export type AccountantType = {
+    cell: string;
+    name: {
+        first: string;
+        last: string;
+    };
+    email: string;
+    picture: { thumbnail: string; medium: string };
+    login: {
+        uuid: string;
+    };
+};
+export type AccountantResponseType = {
+    results: AccountantType[];
+    info: {
+        page: number;
+    };
+};
