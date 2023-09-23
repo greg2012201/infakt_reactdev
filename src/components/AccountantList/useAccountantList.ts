d698a47f198a54afd90548a8c7bdf802bf180a2e
Grzegorz Dubiel <greg2012201@gmail.com>
2023-09-24 01:22:44 +0200

    Add hook for managing data flow.

---
+++ src/components/AccountantList/useAccountantList.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { AccountantResponseType } from "../../types";
+import useFetch from "../../hooks/useFetch";
+
+const useAccountantList = () => {
+    const [data, setData] = useState<AccountantResponseType>();
+    const { isLoading, error, fetchData } = useFetch();
+
+    const fetchPage = () => {
+        fetchData<AccountantResponseType>({
+            fetcher: async () =>
+                await fetch(
+                    `https://randomuser.me/api/?seed=abc&gender=female&page=${(data?.info?.page ?? 0) + 1}&results=5`,
+                    { method: "GET" }
+                ),
+            onSuccess: (res) => {
+                setData((prevState) => {
+                    if (!prevState?.results?.length) {
+                        return res;
+                    }
+                    return {
+                        results: [...prevState.results, ...res.results],
+                        info: { ...prevState.info, ...res.info },
+                    };
+                });
+            },
+        });
+    };
+
+    useEffect(() => {
+        if (data?.results?.length || isLoading) {
+            return;
+        }
+        fetchPage();
+    }, []);
+
+    return {
+        isLoading,
+        isLoadingMore: isLoading && !!data?.results?.length,
+        error,
+        accountants: data?.results ?? [],
+        fetchMore: fetchPage,
+    };
+};
+export default useAccountantList;
