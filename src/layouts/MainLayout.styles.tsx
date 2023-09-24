eeb3c08e068020906dbd5a33135d680891764c64
Grzegorz Dubiel <greg2012201@gmail.com>
2023-09-24 13:00:37 +0200

    Create main layout.

---
+++ src/layouts/MainLayout.styles.tsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 48px;
+    padding: 48px 0 0 84px;
+`;
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+import { Main } from "./MainLayout.styles";
+
+type Props = {
+    children: ReactNode;
+};
+
+export const MainLayout = ({ children }: Props) => {
+    return (
+        <Main>
+            <img alt="inFakt logo" width="146px" height="38px" src="./images/logo-infakt.svg" />
+            {children}
+        </Main>
+    );
+};
